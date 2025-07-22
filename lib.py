439fbbdb7df3e520791689d3c3367c890fde137a
Adam Carrera <carrera.adam0@gmail.com>
2025-07-22 17:04:18 -0500

    update typing for library functions and tests

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from numpy import datetime64
 from datetime import date
-from typing import Any
 
 TODAYS_DATE = date.today()
 
@@ -13,5 +13,10 @@ def days_left(expiration_date: date) -> int:
     return (expiration_date - TODAYS_DATE).days
 
 
-def format_datetime(date_str: Any) -> date:
+def format_datetime(date_str: datetime64) -> date:
     return pd.Timestamp(date_str).date()
+
+
+def generate_report(df: pd.DataFrame) -> str:
+    """print each item and how many days until it expires, sorted by days left"""
+    return df.loc[:, ["Name", "Days Left"]].sort_values(by="Days Left").to_string()
--- tests/test_is_expired.py
+++ tests/test_is_expired.py
@@ -1,7 +1,10 @@
-from lib import is_expired, TODAYS_DATE, format_datetime
+from lib import is_expired, TODAYS_DATE
+from datetime import date
 
-past_date = format_datetime("08/08/1990")
-future_date = format_datetime("12/02/2077")
+# past_date = date("08/08/1990")
+past_date = date(month=8, day=8, year=1990)
+# future_date = date("12/02/2077")
+future_date = date(month=12, day=2, year=2077)
 
 
 def test_past_date() -> None:
