16dc6ddb208748195d38803b98c3105d06bd5215
Adam Carrera <carrera.adam0@gmail.com>
2025-07-28 16:51:56 -0500

    refactor report generation; add functions for expired and expiring foods, and update logging format

--- lib.py
+++ lib.py
@@ -1,9 +1,23 @@
+import logging
 import pandas as pd
+
 from numpy import datetime64
 from datetime import date
 from tabulate import tabulate, tabulate_formats
 
 TODAYS_DATE = date.today()
+REPORT_HEADERS = ["Name", "Days Left"]  # headers to include in report
+WARNING_THRESHOLD = 10  # number of days before a food is considered expiring
+TABLE_FORMAT = "simple"  # tabulate table format
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="report.log",
+    filemode="w",
+    format="%(message)s",
+)
 
 
 def is_expired(expiration_date: date) -> bool:
@@ -18,15 +32,35 @@ def format_datetime(date_str: datetime64) -> date:
     return pd.Timestamp(date_str).date()
 
 
-def generate_report(df: pd.DataFrame, tablefmt: str) -> str:
-    """print each item and how many days until it expires, sorted by days left"""
-
+def format_table(table: pd.DataFrame, tablefmt: str) -> str:
     if tablefmt not in tabulate_formats:
         raise TypeError(f"Table format not compatible, expected: {tabulate_formats}")
 
-    df = df.loc[:, ["Name", "Days Left"]].sort_values(by="Days Left")
-    table = tabulate(
-        df.to_dict(orient="list"), headers="keys", tablefmt=tablefmt, showindex=False
+    return tabulate(
+        table.to_dict(orient="list"),
+        headers="keys",
+        tablefmt=tablefmt,
+        showindex=False,
     )
 
-    return table
+
+def get_expiring_foods(df: pd.DataFrame) -> str:
+    """
+    return foods with less than a certain number of days, but exclude expired foods
+    """
+    table = df.loc[
+        (df["Days Left"] < WARNING_THRESHOLD) & (df["Days Left"] > 0),
+        REPORT_HEADERS,
+    ]
+
+    return format_table(table=table, tablefmt=TABLE_FORMAT)
+
+
+def get_expired_foods(df: pd.DataFrame) -> str:
+    table = df.loc[df["Is Expired"], REPORT_HEADERS]
+    return format_table(table=table, tablefmt=TABLE_FORMAT)
+
+
+def sort_by_expriy_date(df: pd.DataFrame) -> str:
+    table = df.loc[:, REPORT_HEADERS].sort_values(by="Days Left")
+    return format_table(table=table, tablefmt=TABLE_FORMAT)
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import logging
 
 import pandas as pd
-from lib import days_left, format_datetime, is_expired, generate_report
+
+from lib import (
+    days_left,
+    format_datetime,
+    is_expired,
+    get_expired_foods,
+    get_expiring_foods,
+    sort_by_expriy_date,
+    TODAYS_DATE,
+    WARNING_THRESHOLD,
+)
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.DEBUG,
-    # filename="myapp.log",
-    # filemode="w",
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="report.log",
+    filemode="w",
+    format="%(message)s",
 )
 
 
@@ -21,7 +31,26 @@ def main() -> None:
     df["Is Expired"] = df["Expiration Date"].apply(is_expired)
     df["Days Left"] = df["Expiration Date"].apply(days_left)
 
-    logger.info(generate_report(df, "grid"))
+    logging.info(f"Mick and Adam's Kitchen Inventory Report for {TODAYS_DATE}!\n\n")
+
+    # expired_table = get_expired_foods(df)
+    if expired_table := get_expired_foods(df):
+        logging.info("The following foods are past the 'best by' date:")
+        logging.info(expired_table + "\n\n")
+    else:
+        logging.info("No foods have expired\n\n")
+
+    # expiring_table = get_almost_expired_foods(df)
+    if expiring_table := get_expiring_foods(df):
+        logging.info(
+            f"The following foods have less than {WARNING_THRESHOLD} days left:"
+        )
+        logging.info(expiring_table + "\n\n")
+    else:
+        logging.info("No foods are close to expiring\n\n")
+
+    logging.info("Kitchen Inventory:")
+    logging.info(sort_by_expriy_date(df))
 
 
 if __name__ == "__main__":
